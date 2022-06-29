ced98c9019d95ef1205d7f6ee4e25b8db86e7605
Zongxing Hu <huzongxing0826@gmail.com>
2022-06-29 04:03:40 +0000

    [dingo] update logback configuration about driver and sqlline

--- container/images/templates/bin/start-driver.sh
+++ container/images/templates/bin/start-driver.sh
@@ -19,9 +19,8 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 JAR_PATH=$(find $ROOT -name dingo-cli-*.jar)
 
 java ${JAVA_OPTS} \
-    -Dlog4j.configuration=file:${ROOT}/conf/log4j-driver.properties \
+    -Dlogback.configurationFile=file:${ROOT}/conf/logback-driver.xml \
     -classpath ${JAR_PATH} \
     io.dingodb.cli.Tools driver \
     --config ${ROOT}/conf/client.yaml \
     $@ > ${ROOT}/log/driver.out
-
