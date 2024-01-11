f7d69382e6e7e3151efee0a6ef7f486a627cba4a
java_jack <jack.li@bitvenus.me>
2024-01-11 14:05:44 +0800

    [chore][deploy] add update service config shell to merge_dingo.sh

--- artifacts/merge_dingo.sh
+++ artifacts/merge_dingo.sh
@@ -16,6 +16,16 @@ if [ -d dingo/dingo ]; then
 fi
 cd dingo
 cp ${NEW_DIR}/sqlline-*-SNAPSHOT-jar-with-dependencies.jar libs/
+# sed修改store-gflags.conf、index-gflags.conf、coordinator-gflags.conf的变量值
+#sed -i 's/-min_system_disk_capacity_free_ratio=0.05/-min_system_disk_capacity_free_ratio=0.05/g' conf/store-gflags.conf
+#sed -i 's/-min_system_memory_capacity_free_ratio=0.10/-min_system_memory_capacity_free_ratio=0.10/g' conf/store-gflags.conf
+#sed -i 's/-storage_worker_num=32/-storage_worker_num=32/g' conf/store-gflags.conf
+#
+#sed -i 's/-min_system_disk_capacity_free_ratio=0.05/-min_system_disk_capacity_free_ratio=0.05/g' conf/index-gflags.conf
+#sed -i 's/-min_system_memory_capacity_free_ratio=0.10/-min_system_memory_capacity_free_ratio=0.10/g' conf/index-gflags.conf
+#sed -i 's/-storage_worker_num=16/-storage_worker_num=16/g' conf/index-gflags.conf
+#
+#sed -i 's/-max_hnsw_memory_size_of_region=2147483648/-max_hnsw_memory_size_of_region=2147483648/g' conf/coordinator-gflags.conf
 tar -czvf ../dingo.tar.gz ./*
 cd ..
 rm -rf dingo
